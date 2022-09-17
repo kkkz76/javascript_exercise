5728d623b9fbff7c7b43af25708eb4bc67833da7
kkkz <khantkokozaw382000@gmail.com>
2022-09-17 20:34:45 +0630

    functions

---
+++ difference.js
@@ -0,0 +1,10 @@
+function diff(a,b){
+    return Math.abs(b-a);
+}
+console.log(diff(10,100));
+
+const diff1 = function(a,b){
+    return Math.abs(b-a);
+}
+console.log(diff1(10,100));
+
---
+++ function.js
@@ -0,0 +1,11 @@
+
+function logFuncName(a){
+   console.log("Function Name:",a.name);
+
+}
+
+const greeting = function(){
+    console.log('hello');
+}
+
+logFuncName(greeting)
