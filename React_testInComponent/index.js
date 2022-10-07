e59ab915e26fee15661d58f1d1a5f011b7f98f01
khantko kozaw <khantkokozaw382000@gmail.com>
2022-10-07 13:08:59 +0800

    testInComponent

---
+++ React_testInComponent/index.js
@@ -0,0 +1,14 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function HelloWorld(){
+    return <h1>
+        Nay kg lr
+        ma kg boo
+    </h1>
+}
+
+
+
+
+root.render(<HelloWorld />);
+
