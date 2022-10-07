9751443524323de78c05291aa6b9ac3d6c87803a
khantko kozaw <khantkokozaw382000@gmail.com>
2022-10-07 13:49:13 +0800

    react_hw

--- testWithBoostrap/index.js
+++ testWithBoostrap/index.js
@@ -2,24 +2,38 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function NavBar(){
     return (
-        <nav className="navbar navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      <img src="https://getbootstrap.com/docs/4.6/assets/brand/bootstrap-social-logo.png" alt="" width="30" height="24" className="d-inline-block align-text-top" />
-      Bootstrap
-    </a>
-  </div>
-</nav>
+        <div class="text-center">
+        <img src="react_1.png" class="rounded "  />
+        <h1>React.JS</h1>
+      </div>
+
+      
+   
     )
 
 }
 
 function Hero(){
-    return <h1>This is hero Section</h1>
+    return (
+        <div class="text-left">
+       <h1>Main Feature of React JS</h1>
+       <ul>
+         <li>Composable</li>
+         <li>JSX</li>
+         <li>Declarative</li>
+       </ul>
+      </div>
+
+      
+   
+    )
+    
+    
+    
 }
 
 function Footer(){
-    return <h1>This is footer Section</h1>
+    return <h5 class="bg-dark text-white p-3 text-center">Created by LTE Member 2022, All Right Reserved.</h5>
 }
 
 function Page(){
