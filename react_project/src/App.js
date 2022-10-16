60c992223b42b85e89701d78f0df0dff03ae9c6a
khantko kozaw <khantkokozaw382000@gmail.com>
2022-10-16 11:28:10 +0800

    react_implement

---
+++ react_project/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './Header';
+
+
+function App() { 
+  return ( 
+  <>
+  <div>Hello Mario</div>
+  <Footer/>
+  <Header/>
+ 
+ 
+  </>
+  );
+ 
+  
+}
+export default App;
