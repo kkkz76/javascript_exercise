92d4b8cacaf07fac744f244673234b749723914b
kkkz76 <khantkokozaw382000@gmail.com>
2022-11-25 16:37:48 +0800

    react_async_revision

---
+++ promise.js
@@ -0,0 +1,19 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+const url= "https://api.github.com/search/users?q=kkkz";
+
+// const fetch_result = fetch(url);
+
+// fetch_result.then(response => response.json()).then(json => console.log(json))
+
+
+// async_await
+const fetchData =async(url) => {
+    const res = await fetch(url);
+    const data =await res.json();
+    console.log(data);
+}
+
+fetchData(url);
